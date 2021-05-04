7532f8d934b98a72e4e3020c3e0c7ef0321f4764
Anatoliy Yuminov <kerjackov.sochi2014@yandex.ru>
2021-05-04 18:20:04 +0300

    practical task 3. Doors and collectible items

---
+++ Assets/SampleScene assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private string AmmoBox;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Item collected: " + AmmoBox);
+        Destroy(gameObject);
+    }
+}
